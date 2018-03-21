364bf5d8b51a5f2980e3fb97253f62d7bec85503
1412368500 <1412368500@qq.com>
2018-03-21 10:48:08 +0800

    servlet集成

--- src/main/java/com/lantw/demo/DemoApplication.java
+++ src/main/java/com/lantw/demo/DemoApplication.java
@@ -3,12 +3,23 @@ package com.lantw.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@ServletComponentScan //添加的注解
 @SpringBootApplication
 @MapperScan("com.lantw.demo.mapper")
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+       return application.sources(DemoApplication.class);
+    }
+	
+
 }
