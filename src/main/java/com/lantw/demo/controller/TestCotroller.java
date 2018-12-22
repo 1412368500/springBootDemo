5b7c6d02d14c04f6a74c58760606f643384d06e9
1412368500 <1412368500@qq.com>
2018-12-22 17:02:19 +0800

    获取信息

--- src/main/java/com/lantw/demo/controller/TestCotroller.java
+++ src/main/java/com/lantw/demo/controller/TestCotroller.java
@@ -1,20 +1,30 @@
 package com.lantw.demo.controller;
 
+import com.lantw.demo.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lantw.demo.service.UserService;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TestCotroller {
 	
 	@Autowired
 	private UserService userService;
-	
-	@GetMapping("/")
-    public Object home() {
-//		return "241412";
-		return userService.selectByPrimaryKey(1l);
-    }
+
+	@RequestMapping("/user/list")
+	public ModelAndView listUser(Model model) {
+		List<User> userList = new ArrayList<>();
+        userList.add(userService.selectByPrimaryKey(1l));
+
+		model.addAttribute("users", userList);
+		return new ModelAndView("/user/list","model",model);
+	}
 }
